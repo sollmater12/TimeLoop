c43be137215d7a0bef666441fb962b61100e0af8
sollametr <kuba.andrey.jr@yandex.ru>
2021-12-29 17:23:06 +0300

    - add start screen

--- main.py
+++ main.py
@@ -1,2 +1,62 @@
-def sum(a, b):
-    return a + b
+import sys
+import os
+import pygame
+
+FPS = 60
+SIZE = WIDTH, HEIGHT = 500, 500
+SCREEN = pygame.display.set_mode(SIZE)
+CLOCK = pygame.time.Clock()
+
+
+def load_image(filename, colorkey=None):
+    fullname = os.path.join("data", filename)
+    if not os.path.isfile(fullname):
+        print('File is not found!')
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def start_screen():
+    intro_text = ["TimeLoop", "",
+                  "Правила игры",
+                  "Если в правилах несколько строк,",
+                  "приходится выводить их построчно"]
+    fon = pygame.transform.scale(SCREEN, (WIDTH, HEIGHT))
+    SCREEN.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('white'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        SCREEN.blit(string_rendered, intro_rect)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                pass  # начинаем игру
+        pygame.display.flip()
+        CLOCK.tick(FPS)
+
+
+if __name__ == '__main__':
+    pygame.init()
+    start_screen()
