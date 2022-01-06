92e7e4717e4d02666dec7037a91f63fbed8479a9
vanyadr <vanyadr@yandex.ru>
2022-01-06 13:42:42 +0300

    add coin and kill blocks

--- main.py
+++ main.py
@@ -25,6 +25,10 @@ all_sprites = pygame.sprite.Group()
 tiles_group = pygame.sprite.Group()
 player_group = pygame.sprite.Group()
 tele_group = pygame.sprite.Group()
+kill_group = pygame.sprite.Group()
+good_blocks = pygame.sprite.Group()
+fon = pygame.sprite.Group()
+coin = pygame.sprite.Group()
 
 CLOCK = pygame.time.Clock()
 CONNECTION = Connection()
@@ -75,21 +79,21 @@ class Player(pygame.sprite.Sprite):
 
     def move(self):
         self.acc = vec(0, 0.1)
-        pressed_keys = pygame.key.get_pressed()
-
-        if pressed_keys[K_LEFT]:
-            self.acc.x = -ACC
-        if pressed_keys[K_RIGHT]:
-            self.acc.x = ACC
-
-        self.acc.x += self.vel.x * FRIC
+        # pressed_keys = pygame.key.get_pressed()
+        #
+        # if pressed_keys[K_LEFT]:
+        #     self.acc.x = -ACC
+        # if pressed_keys[K_RIGHT]:
+        #     self.acc.x = ACC
+        #
+        # self.acc.x += self.vel.x * FRIC
         self.vel += self.acc
         self.pos += self.vel + 0.5 * self.acc
 
-        if self.pos.x > WIDTH:
-            self.pos.x = 0
-        if self.pos.x < 0:
-            self.pos.x = WIDTH
+        # if self.pos.x > WIDTH:
+        #     self.pos.x = 0
+        # if self.pos.x < 0:
+        #     self.pos.x = WIDTH
         self.rect.midbottom = self.pos
 
     def update(self, hits):
@@ -98,7 +102,7 @@ class Player(pygame.sprite.Sprite):
             self.vel.y = 0
 
     def get_coord(self):
-        return [self.pos_x, self.pos_y]
+        return [self.pos.x, self.pos.y]
 
     def jump(self):
         hits = pygame.sprite.spritecollide(self, tiles_group, False)
@@ -110,7 +114,7 @@ class Field(pygame.sprite.Sprite):
     image = pygame.transform.scale(load_image('check_draw.png'), (50, 13))
 
     def __init__(self, fls):
-        super().__init__(tiles_group)
+        super().__init__(tiles_group, good_blocks, all_sprites)
         self.image = Field.image
         self.rect = self.image.get_rect()
         self.fls = fls
@@ -177,7 +181,7 @@ class Teleport(pygame.sprite.Sprite):
     image = load_image("tile1.png")
 
     def __init__(self, fls):
-        super().__init__(tiles_group)
+        super().__init__(tele_group, good_blocks, all_sprites)
         self.image = Teleport.image
         self.rect = self.image.get_rect()
         self.fls = fls
@@ -204,14 +208,41 @@ class Teleport(pygame.sprite.Sprite):
         return self.fls
 
 
+class Killer(pygame.sprite.Sprite):
+    image = pygame.transform.scale(load_image('tile_kill2.png'), (150, 26))
+
+    def __init__(self):
+        super().__init__(kill_group, all_sprites)
+        self.image = Killer.image
+        self.rect = self.image.get_rect()
+        self.rect.x = 5
+        self.rect.y = random.randrange(60, 880)
+        while pygame.sprite.spritecollide(self, good_blocks, False):
+            self.rect.y = random.randrange(60, 880)
+        direc = [-1, 1]
+        self.check_x = random.choice(direc)
+        self.vx = 1
+        self.image = Killer.image
+
+    def update(self, *args, **kwargs) -> None:
+        if self.rect.x < 350 and self.check_x == 1:
+            self.rect.x += self.vx
+        if self.rect.x == 350 and self.check_x == 1:
+            self.check_x = -1
+        if self.rect.x > 5 and self.check_x == -1:
+            self.rect.x -= self.vx
+        if self.rect.x == 5 and self.check_x == -1:
+            self.check_x = 1
+
+
 class Lava(pygame.sprite.Sprite):
-    image = load_image("lava.png")
-    image1 = load_image("lava1.png")
+    image = load_image("lava1.png")
+    image1 = load_image("lava2.png")
     flag = True
     count = 0
 
     def __init__(self):
-        super().__init__(tiles_group)
+        super().__init__(all_sprites)
         self.image = Lava.image1
         self.rect = self.image.get_rect()
         self.rect.x = 0
@@ -223,6 +254,18 @@ class Lava(pygame.sprite.Sprite):
     def update(self, *args, **kwargs) -> None:
         self.rect.y -= self.vx / FPS
         CLOCK.tick(FPS)
+
+
+class Coin(pygame.sprite.Sprite):
+    image = pygame.transform.scale(load_image('cn.png'), (50, 50))
+
+    def __init__(self):
+        super().__init__(coin, all_sprites)
+        self.image = Coin.image
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(5, 450)
+        self.rect.y = random.randrange(100, 850)
+        self.image = Coin.image
 
 
 def show_start_text():
@@ -274,6 +317,13 @@ def show_records():
     font = pygame.font.Font(None, 30)
     string_rendered = font.render(text, 1, pygame.Color(0, 0, 0))
     SCREEN.blit(string_rendered, (180, 200))
+
+
+# def res(step):
+#     text = f'{step} m'
+#     font = pygame.font.Font(None, 30)
+#     string_rendered = font.render(text, 1, pygame.Color(0, 0, 0))
+#     SCREEN.blit(string_rendered, (10, 100))
 
 
 def show_start_label():
@@ -511,7 +561,7 @@ def draw_buttons_2(manager):
 
 
 def end():
-    global all_sprites, tiles_group, lava_group, player_group
+    global all_sprites, tiles_group, player_group
     die = load_image('die.png')
     SCREEN.blit(die, (0, 0))
     pygame.display.flip()
@@ -524,22 +574,24 @@ def end():
                 a = False
                 all_sprites = pygame.sprite.Group()
                 tiles_group = pygame.sprite.Group()
-                lava_group = pygame.sprite.Group()
                 player_group = pygame.sprite.Group()
                 main_game()
             if event.type == pygame.MOUSEBUTTONDOWN and 255 <= event.pos[0] <= 340 and 370 <= event.pos[1] <= 450:
                 a = False
                 all_sprites = pygame.sprite.Group()
                 tiles_group = pygame.sprite.Group()
-                lava_group = pygame.sprite.Group()
                 player_group = pygame.sprite.Group()
                 start_screen()
 
 
+count = 0
+count1 = 0
+
+
 def main_game():
-    global all_sprites, tiles_group, lava_group, player_group
+    global all_sprites, tiles_group, player_group, count, coin, kill_group, tele_group, count1
     SCREEN = pygame.display.set_mode(SIZE_2)
-    fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH_2, HEIGHT_2))
+    fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH, HEIGHT))
     hat = load_image('hat.png')
     hat0 = load_image('hat0.png')
     hat1 = load_image('hat1.png')
@@ -554,29 +606,37 @@ def main_game():
         if i == 11:
             field = Teleport(fls)
             tele_group.add(field)
+            good_blocks.add(field)
             fls = field.ret_fls()
         else:
             field = Field(fls)
             tiles_group.add(field)
+            good_blocks.add(field)
             fls = field.ret_fls()
     print(fls)
+    for i in range(random.randrange(1, 4)):
+        kill = Killer()
     player = Player(fls[0][0], fls[0][1])
+    # coord = player.get_coord()[1]
+    cn = Coin()
     lava = Lava()
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate()
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    player.change_direction(-1, 0)
-                if event.key == pygame.K_RIGHT:
-                    player.change_direction(1, 0)
-                if event.key == pygame.K_UP:
-                    player.change_direction(0, -1)
-                if event.key == pygame.K_DOWN:
-                    player.change_direction(0, 1)
+                # if event.key == pygame.K_LEFT:
+                #     player.change_direction(-1, 0)
+                # if event.key == pygame.K_RIGHT:
+                #     player.change_direction(1, 0)
+                # if event.key == pygame.K_UP:
+                #     player.change_direction(0, -1)
+                # if event.key == pygame.K_DOWN:
+                #     player.change_direction(0, 1)
                 if event.key == pygame.K_SPACE:
                     player.jump()
+                    # count1 += abs(player.get_coord()[1] - 900 + coord)
             if event.type == pygame.MOUSEBUTTONDOWN and 410 <= event.pos[0] <= 470 and 0 <= event.pos[1] <= 50:
                 SCREEN.blit(hat0, (0, 0))
                 SCREEN.blit(hat1, (0, 0))
@@ -588,17 +648,25 @@ def main_game():
                 SCREEN.blit(stp, (0, 0))
                 pygame.display.flip()
                 stop_menu()
-            if event.type == pygame.MOUSEBUTTONDOWN and 359 <= event.pos[0] <= 507 and 683 <= event.pos[1] <= 900:
+            if (event.type == pygame.MOUSEBUTTONDOWN and 359 <= event.pos[0] <= 507 and 683 <= event.pos[1] <= 900)\
+                    or (event.type == pygame.KEYDOWN and event.key == pygame.K_l):
                 if b:
                     b = False
+                    # count1 += abs(player.get_coord()[1] - 900 + coord)
                 else:
                     b = True
+                    # count1 += abs(player.get_coord()[1] - 900 + coord)
         if pygame.sprite.collide_mask(player, lava):
             return end()
+        if player.get_coord()[1] > 943:
+            return end()
+        if pygame.sprite.spritecollide(player, kill_group, False):
+            return end()
+        if pygame.sprite.spritecollide(player, coin, True):
+            count += 1
         if pygame.sprite.spritecollide(player, tele_group, False):
             all_sprites = pygame.sprite.Group()
             tiles_group = pygame.sprite.Group()
-            lava_group = pygame.sprite.Group()
             player_group = pygame.sprite.Group()
             main_game()
         SCREEN.blit(fon, (0, 0))
@@ -606,16 +674,18 @@ def main_game():
         if b:
             player.move()
             player.update(pygame.sprite.spritecollide(player, tiles_group, False))
-            player_group.draw(SCREEN)
-            tiles_group.draw(SCREEN)
+            all_sprites.draw(SCREEN)
             SCREEN.blit(clc, (430, 790))
         else:
-            tiles_group.update()
-            player_group.draw(SCREEN)
-            tiles_group.draw(SCREEN)
+            good_blocks.update()
+            kill_group.update()
+            lava.update()
+            all_sprites.draw(SCREEN)
             SCREEN.blit(clc1, (430, 790))
+        # res(count1)
         CLOCK.tick(FPS)
         pygame.display.flip()
+        print(count)
 
 
 if __name__ == '__main__':
